8dcc96ce3cefec0d16da52608b5a7f800c0d6410
projetoswsg-ux <projetoswsg@gmail.com>
2026-09-19 22:24:18 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://guia-do-autopropelido.vercel.app',
+  integrations: [sitemap()],
   output: 'static',
 });
